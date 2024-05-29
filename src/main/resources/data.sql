8d1125b551f04e21362ca9d3dbde5a37977c72e0
leandroaebi <leandro.aebi@bbzbl-it.ch>
2024-05-29 11:19:24 +0200

    add sample rental datas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,3 +14,14 @@ VALUES
     ('Mercedes-Benz', 'C-Class', 'Silver', 'JKL012', 22000, 160, 'Diesel', 5, 75, 5, true),
     ('Toyota', 'Yaris', 'Red', 'MNO345', 12000, 90, 'Hybrid', 5, 35, 3, true),
     ('Ford', 'Fiesta', 'Green', 'PQR678', 25000, 95, 'Petrol', 5, 40, 5, true);
+
+
+-- Sample Data for the rentals
+INSERT INTO rentals (car_id, user_id, rental_start, rental_end, total_cost, status)
+VALUES
+    (1, 1, '2024-06-01', '2024-06-10', 459, 'completed'),
+    (2, 2, '2024-07-01', '2024-07-15', 989, 'active'),
+    (3, 3, '2024-08-01', '2024-08-07', 391, 'reserved'),
+    (4, 1, '2024-09-01', '2024-09-05', 379, 'completed'),
+    (5, 2, '2024-10-01', '2024-10-20', 719, 'active'),
+    (6, 3, '2024-11-01', '2024-11-10', 409, 'reserved');
